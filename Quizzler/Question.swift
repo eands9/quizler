5773d46c7b8631b4890d6776a0ffadd2342250f8
Eric Hernandez <eands9@yahoo.com>
2018-12-29 18:27:42 -0600

    Quizzler-master

--- Quizzler/Question.swift
+++ Quizzler/Question.swift
@@ -11,10 +11,13 @@ class Question {
     
     let questionText : String
     let answer : Bool
+    
     init(text: String, correctAnswer: Bool) {
         questionText = text
         answer = correctAnswer
         
     }
 }
+
+
 
